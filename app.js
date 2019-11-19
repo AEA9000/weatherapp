e4189fa76e694b5a5ab58b3d5d51dd8b2237ede5
Adel <adelelallaoui83@gmail.com>
2019-11-19 10:39:57 +0100

    stijnsfix

---
+++ app.js
@@ -0,0 +1,45 @@
+window.addEventListener('load', () => {
+    let long;
+    let latitude;
+    let temparatureDescription = document.querySelector('.temperature-discription');
+    let temperatureDegree = document.querySelector('temperature-degree');
+    let locationTimezone = document.querySelector('temperature-timezone');
+
+
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(position => {
+            long = position.coords.longitude;
+            lat = position.coords.latitude;
+
+            const api = `http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=911d0657f9e0a1f3676c3a63ad19ce49`;
+
+            fetch(api)
+                .then(response => {
+                    return response.json();
+                })
+                .then(data => {
+                    console.log(data);
+                    console.log(data.list.length);
+
+                    for (i = 0; i < data.list.length; i++) {
+                        console.log(i);
+                        if (i === 0) {
+                            let first = data.list[i].main.temp;
+                            console.log(first);
+                            let temp1 = document.querySelector(".temperatureDegree1");
+                            temp1.innerHTML = first;
+                        }
+                        if (i === 8) {
+                            let second = data.list[i].main.temp;
+                            console.log(second);
+                            let temp2 = document.querySelector(".temperatureDegree2");
+                            temp2.innerHTML = second;
+                        }
+                    }
+
+                });
+        });
+    }
+
+
+});
